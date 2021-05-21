80cb58cae8f682f23b7b6f58b92b44fe98e4c58e
karaware <ping.meiron@gmail.com>
2021-05-21 13:53:38 +0900

    first comment

---
+++ tweet_liked.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tweepy
+import time
+
+CK = 'F9775TKQrUgwws9uJm7qr0bw9'
+CS = 'UDPVHjwVMaOf3HMHJ2U68s2ssPxv5Kf9LAePW3O2x2n3baSV7V'
+AT = '1384009908178198529-g8yEdNsWEhlsxCEsjqIhOaoAdFnLUP'
+AS = 'RLT6juFbVbPcHHe3V9ERbjSvYmeZmc68Ppcrlb3OPUftr'
+
+auth = tweepy.OAuthHandler(CK, CS)
+auth.set_access_token(AT, AS)
+api = tweepy.API(auth)
+
+search_list = ['#家庭菜園', '#プランター菜園', '#ベランダ菜園', '#花', '#TLを花でいっぱいにしよう']
+tweet_count = 3
+
+for search in search_list:
+    print('Searching... {}' .format(search))
+    # サーチ結果 #
+    search_result = api.search(q=search, count=tweet_count)
+    for tweet in search_result:
+        tweet_id = tweet.id
+        user = tweet.user.screen_name
+        print(user)
+        if user != "GTimerapps":
+            try:
+                # いいねの処理 #
+                api.create_favorite(id=tweet_id)
+                print('Tweet_liked')
+                time.sleep(4)
+            except tweepy.TweepError as e:
+                print(e.reason)
+            except StopIteration:
+                break
+
+
+
